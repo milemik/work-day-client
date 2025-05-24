71d5f2c425cb3b4c0e7b68749d272214869f6a30
Ivan Miletic <milemik68@gmail.com>
2025-05-24 11:33:17 +0200

    Refactor NavBar component styling for improved aesthetics and layout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,13 @@
 import NavBarLink from "./NabBarLink";
 
-
 export default function NavBar() {
     return (
-        <nav className="bg-gray-400">
-            <ul className="flex gap-4 text-3xl">
-                <NavBarLink toVal={"/"} text={"Home"}/>
-                <NavBarLink toVal={"work-days/"} text={"WorkDays"}/>
-                <NavBarLink toVal={"companies/"} text={"Companies"}/>                  
+        <nav className="bg-gradient-to-r from-blue-600 to-blue-400 shadow-md py-4">
+            <ul className="flex justify-center gap-8 text-xl font-semibold">
+                <NavBarLink toVal={"/"} text={"Home"} />
+                <NavBarLink toVal={"work-days/"} text={"WorkDays"} />
+                <NavBarLink toVal={"companies/"} text={"Companies"} />
             </ul>
         </nav>
-    )
+    );
 }
