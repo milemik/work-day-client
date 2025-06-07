df310bf9f38f357718a61275b30b923a915408d2
milemik <milemik68@gmail.com>
2025-06-07 15:48:56 +0200

    UI navbar active link update
    Now active link text will have white color

--- src/components/NabBarLink.jsx
+++ src/components/NabBarLink.jsx
@@ -2,6 +2,6 @@ import { NavLink } from "react-router";
 
 export default function NavBarLink({text, toVal}) {
     return (
-        <li><NavLink to={toVal} className={({isActive}) => isActive ? "text-amber-200" : ""}>{text}</NavLink></li>
+        <li><NavLink to={toVal} className={({isActive}) => isActive ? "text-white" : ""}>{text}</NavLink></li>
     )
 }
